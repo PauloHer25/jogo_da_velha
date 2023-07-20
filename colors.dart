8119382b04780ba19d35004a76b43e0b7af834df
PauloHer25 <36310184+PauloHer25@users.noreply.github.com>
2023-07-20 10:29:57 -0300

    Delete colors.dart

--- colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MainColor {
-  static Color primaryColor = Color(0xFF4169E8);
-  static Color secondaryColor = Color(0xFFffca27);
-  static Color accentColor = Color(0xFFff4B4B);
-}
